bdfe6cdfaa94c3796b0326aff7e168615d8c6bf7
sutan mufti <upsutan@gmail.com>
2023-09-05 21:39:27 +0100

    image loads

--- script/immutable/chunks/paths.9c6bc97e.js
+++
@@ -1,1 +0,0 @@
-var s;const t=((s=globalThis.__sveltekit_kp2ol8)==null?void 0:s.base)??"/about-sutan";var a;const e=((a=globalThis.__sveltekit_kp2ol8)==null?void 0:a.assets)??t;export{e as a,t as b};
