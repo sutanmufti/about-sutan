1d13b2ba26a85c82d0f10a686a61a5168724028d
sutan mufti <upsutan@gmail.com>
2023-09-05 07:26:55 +0100

    renaming dir

--- _app/immutable/chunks/paths.7272134d.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_mo3nme)==null?void 0:s.base)??"/personal-site";var e;const t=((e=globalThis.__sveltekit_mo3nme)==null?void 0:e.assets)??a;export{t as a,a as b};
